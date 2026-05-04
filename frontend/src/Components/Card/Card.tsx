131c2139f0b2de0ad94dc1d63bbff213f313ac9a
VictorFagundes07 <victorolentino5@gmail.com>
2026-05-04 09:52:26 -0300

    fim do video 4

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { JSX } from 'react'
 import "./Card.css";
 
 interface Props {
@@ -7,18 +7,18 @@ interface Props {
   price: number;
 }
 
-const Card = ({companyName, ticker, price}: Props) => {
-  return <div className = "card">
-    
-    <div className='details'>
-      <h2>{companyName} ({ticker})</h2>
-      <p>{price}</p>
+const Card: React.FC<Props> = ({companyName, ticker, price}: Props) : JSX.Element  => {
+  return (
+    <div className = "card">
+     <div className='details'>
+       <h2>{companyName} ({ticker})</h2>
+       <p>{price}</p>
+     </div>
+     <p className='info'>
+       blalala dakdakdakdadawkdladasldmldmakmsdmakdmad,sdmawk
+     </p>
     </div>
-    <p className='info'>
-      blalala dakdakdakdadawkdladasldmldmakmsdmakdmad,sdmawk
-    </p>
-  </div>;
-  
+  );
 };
 
 export default Card
--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { JSX } from 'react'
 import Card from '../Card/Card'
 
 interface Props  {};
 
-const CardList = (props: Props) => {
-  return <div>
-    <Card companyName="Ford" ticker="SLA" price={200} />
-    <Card companyName="tesla" ticker="SLA" price={200} />
-    <Card companyName="ferrari" ticker="SLA" price={200} />
+const CardList: React.FC<Props> = (props: Props) : JSX.Element  => {
+  return(
+   <div>
+     <Card companyName="Ford" ticker="SLA" price={200} />
+     <Card companyName="tesla" ticker="SLA" price={200} />
+     <Card companyName="ferrari" ticker="SLA" price={200} />
     
-  </div>;
+   </div>
+  );
   
 };
 
