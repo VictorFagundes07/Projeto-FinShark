2d804dac803a64bf25f1252d7a4282ca73bad7b5
VictorFagundes07 <victorolentino5@gmail.com>
2026-05-05 12:49:14 -0300

    video 11 terminado

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,24 +1,26 @@
-import React, { JSX } from 'react'
+import React, { JSX } from "react";
 import "./Card.css";
+import { CompanySearch } from "../../company";
 
 interface Props {
-  companyName: string;
-  ticker: string;
-  price: number;
+  id: string;
+  searchResult: CompanySearch;
 }
 
-const Card: React.FC<Props> = ({companyName, ticker, price}: Props) : JSX.Element  => {
+const Card: React.FC<Props> = ({ id, searchResult }: Props): JSX.Element => {
   return (
-    <div className = "card">
-     <div className='details'>
-       <h2>{companyName} ({ticker})</h2>
-       <p>{price}</p>
-     </div>
-     <p className='info'>
-       blalala dakdakdakdadawkdladasldmldmakmsdmakdmad,sdmawk
-     </p>
+    <div key={id} id={id} className="card">
+      <div className="details">
+        <h2>
+          {searchResult.name} ({searchResult.symbol})
+        </h2>
+        <p>{searchResult.currency}</p>
+      </div>
+      <p className="info">
+        {searchResult.exchangeShortName} - {searchResult.stockExchange}
+      </p>
     </div>
   );
 };
 
-export default Card
+export default Card;
--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -1,18 +1,26 @@
-import React, { JSX } from 'react'
-import Card from '../Card/Card'
+import React, { JSX } from "react";
+import Card from "../Card/Card";
+import { CompanySearch } from "../../company";
+import { v4 as uuidv4 } from "uuid";
 
-interface Props  {};
+interface Props {
+  searchResults: CompanySearch[];
+}
 
-const CardList: React.FC<Props> = (props: Props) : JSX.Element  => {
-  return(
-   <div>
-     <Card companyName="Ford" ticker="SLA" price={200} />
-     <Card companyName="tesla" ticker="SLA" price={200} />
-     <Card companyName="ferrari" ticker="SLA" price={200} />
-    
-   </div>
+const CardList: React.FC<Props> = ({ searchResults }: Props): JSX.Element => {
+  return (
+    <>
+      {searchResults.length > 0 ? (
+        searchResults.map((result) => {
+          return (
+            <Card id={result.symbol} key={uuidv4()} searchResult={result} />
+          );
+        })
+      ) : (
+        <h1>No results!</h1>
+      )}
+    </>
   );
-  
 };
 
-export default CardList
+export default CardList;
